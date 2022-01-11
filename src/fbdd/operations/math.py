24bc274c05ccc3bfedd857989d2f44dab0c1286c
Dmitry Mogilevsky <dmitry.mogilevsky@gmail.com>
2022-01-11 20:58:56 +0000

    change col_name to use DataAttribute

--- src/fbdd/operations/math.py
+++ src/fbdd/operations/math.py
@@ -1,8 +1,10 @@
 from typing import Union
 import pandas as pd
 
+from fbdd.definitions.core import DataAttribute
 
-def add_literal(data: pd.DataFrame, col_name: str, literal: Union[str, int, float]) -> pd.DataFrame:
+
+def add_literal(data: pd.DataFrame, col_name: DataAttribute, literal: Union[str, int, float]) -> pd.DataFrame:
     data = data.copy()
-    data[col_name] = literal
+    data[col_name.N] = literal
     return data
