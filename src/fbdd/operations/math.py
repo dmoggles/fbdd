6771c1df48fc838261affcf116492e61c02ce7d3
Dmitry Mogilevsky <dmitry.mogilevsky@gmail.com>
2022-01-11 20:40:37 +0000

    removing literal

--- src/fbdd/operations/math.py
+++ src/fbdd/operations/math.py
@@ -1,8 +1,8 @@
-from typing import Literal
+from typing import Union
 import pandas as pd
 
 
-def add_literal(data: pd.DataFrame, col_name: str, literal: Literal) -> pd.DataFrame:
+def add_literal(data: pd.DataFrame, col_name: str, literal: Union[str, int, float]) -> pd.DataFrame:
     data = data.copy()
     data[col_name] = literal
     return data
