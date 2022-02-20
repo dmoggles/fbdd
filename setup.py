1ff15099ec322ee94faace7cf3f63f0ed7ff9041
Dmitry Mogilevsky <dmitry.mogilevsky@gmail.com>
2022-02-20 13:32:01 +0000

    downgrading mpl take 2

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         "asyncio",
         "tqdm",
         "scipy",
-        "matplotlib==3.3.1",
+        "matplotlib==3.2.2",
         "highlight_text",
         "fuzzywuzzy",
         "python-Levenshtein"
